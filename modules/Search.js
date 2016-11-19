6f877afcb92a4f8e8ba2bebfe7b55b533ccd9002
Sam Harris <s.w.h@live.co.uk>
2016-11-19 12:50:37 +0000

    basic search functionality implemeted

--- modules/Search.js
+++ modules/Search.js
@@ -1,60 +1,63 @@
 import React from 'react'
 import { Link } from 'react-router'
 import NavLink from './NavLink'
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-import FlatButton from 'material-ui/FlatButton';
-
-
-import SearchBar from './SearchBar'
 import Button from './Button'
+import TextField from 'material-ui/TextField';
 import getMuiTheme        from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider   from 'material-ui/styles/MuiThemeProvider';
 import Projects from './Projects';
 import Trending_ideas from './Trending_ideas';
+
+// import IconButton from 'material-ui/IconButton';
+// import NavigationClose from 'material-ui/svg-icons/navigation/close';
+// import FlatButton from 'material-ui/FlatButton';
+// import SearchBar from './SearchBar'
+// import AppBar from 'material-ui/AppBar';
 // import Autocomplete from './Autocomplete'
-import TextField from 'material-ui/TextField';
 
-
-function handleTouchTap() {
-  alert('onTouchTap triggered on the title component');
-}
-
-const styles = {
-  title: {
-    cursor: 'pointer',
-  },
-};
 
 // filter projects to show using search
 
 
+
 export default React.createClass({
+  getInitialState: function () {
+    return { search: '' };
+  },
+
+  handleInput: function (){
+    this.state.search = this.refs.search.input.value
+    console.log(this.state.search);
+  },
   
-  render() {
+  render: function () {
     var DB = this.props.route.initialData
-    console.log(DB)
+    var search = this.state.search
     return (
       <div>
         <h2>Search</h2>
-
-
         <MuiThemeProvider>
-            <TextField id="search" floatingLabelText="Search for projects" fullWidth={true}/>
+            <TextField ref="search" floatingLabelText="Search for projects" fullWidth={true} onKeyPress={this.handleInput}/>
         </MuiThemeProvider>
 
         <MuiThemeProvider>
-          <Button />
+          <Button onClick={this.handleInput} text="Search" />
         </MuiThemeProvider>
-        <ul>
+        <ul role='nav'>
+          <li><NavLink activeClassName="active" to='/search/:name'>Show All</NavLink></li>
+          <li><NavLink activeClassName="active" to='/search/station'>Station</NavLink></li>
+          
         </ul>
-        <Projects initialData={DB}/>
-        <Trending_ideas initialData={DB}/>
+        <div>{this.props.children}</div>
+
+
+        
       </div>
       )	
   }
 })
+        // <Trending_ideas initialData={DB}/>
+        // <Projects initialData={DB} search={search}/>
 
 
         // <NavLink to='/search/:input'>Search for projects</NavLink>
@@ -71,6 +74,16 @@ export default React.createClass({
 //         	  <button type='submit'> Search </button>
 //         	</form>
 
+
+// function handleTouchTap() {
+//   alert('onTouchTap triggered on the title component');
+// }
+
+// const styles = {
+//   title: {
+//     cursor: 'pointer',
+//   },
+// };
 
         // <MuiThemeProvider>
         //   <AppBar>
